e1315a3353d725df4b20e8946fb95a2c00f94058
2012012406 <chenyy539@nenu.edu.cn>
2015-01-04 23:57:16 +0800

    利用委托求积分
    这是一个利用委托求积分的C#程序，积分方法是采用的梯形法

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project8
+{
+    delegate double F(double  x);
+    class Program
+    {
+        public static double fun(double x)
+        {
+            return x * x;
+        }
+        public static double abs(double a)
+        {
+            if (a > 0)
+            {
+                return a ;
+            }
+            else
+            {
+                return -a;
+            }
+        }
+        public static double getArea(double a, double b, F f)
+        {
+            int sign = 1;
+            if (a > b)
+            {
+                double t = a;
+                a = b;
+                b = t;
+                sign = -1;
+            }
+            double h;
+            double x;
+            double sum = 0;
+            int n = 10000;
+            h = abs(a - b) / n;
+            x = a;
+            for (int i = 0; i < n; i++)
+            {
+
+                sum += f(x) + f(x + h);
+                x = x + h;
+            }
+            return sum * h / 2 * sign;
+        }
+        static void Main(string[] args)
+        {
+            double a = 0;
+            double b = 1;
+            F f = new F(Program.fun);
+            double sum = getArea(a, b, f);
+            Console.WriteLine(sum);
+        }
+    }
+}
